9f5583b841daf9ff8dcc750ff875406c423df965
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-02-22 20:54:11 +0900

    Study of Design Pattern

---
+++ AdapterPattern/src/연습문제/Main.java
@@ -0,0 +1,18 @@
+package 연습문제;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        FileIO f = new FileProperties();
+        try {
+            f.readFromFile("file.txt");
+            f.setValue("width", "1024");
+            f.setValue("height", "512");
+            f.setValue("depth", "32");
+            f.writeToFile("newfile.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
