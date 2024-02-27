5884d454b8fad74b21b99cc299cbf28f97be1ffe
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-02-27 21:27:18 +0900

    Study of Design Pattern

--- SingletonPattern/src/Main.java
+++ SingletonPattern/src/Main.java
@@ -12,13 +12,37 @@ public class Main {
             System.out.println("obj1과 obj2는 같은 인스턴스가 아닙니다.");
         }
         System.out.println("End.");
-
+        System.out.println();
         System.out.println("Ticket Start");
         for(int i=0;i<10;i++) {
             System.out.println(i + ":" + TicketMaker.getInstance().getNextTicketNumber());
         }
         System.out.println("End");
 
+        System.out.println();
+        System.out.println("Start.");
+        Triple a1 = Triple.getInstance("ALPHA");
+        Triple b1 = Triple.getInstance("BETA");
+        Triple c1 = Triple.getInstance("GAMMA");
+        Triple a2 = Triple.getInstance("ALPHA");
+        Triple b2 = Triple.getInstance("BETA");
+        Triple c2 = Triple.getInstance("GAMMA");
 
+        if (a1 == a2) {
+            System.out.println("a1 == a2 (" + a1 + ")");
+        } else {
+            System.out.println("a1 != a2");
+        }
+        if (b1 == b2) {
+            System.out.println("b1 == b2 (" + b1 + ")");
+        } else {
+            System.out.println("b1 != b2");
+        }
+        if (c1 == c2) {
+            System.out.println("c1 == c2 (" + c1 + ")");
+        } else {
+            System.out.println("c1 != c2");
+        }
     }
+
 }
